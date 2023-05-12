c2477a7d5e3177c8dff7472b7359329ff0eaa795
Kanniyappan1992 <spkanikings@gmail.com>
2023-05-12 12:06:12 +0530

    dto package updated

--- src/main/java/com/qsp/hotel_billing/dto/Customer.java
+++ src/main/java/com/qsp/hotel_billing/dto/Customer.java
@@ -1,5 +1,30 @@
 package com.qsp.hotel_billing.dto;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cutomerId;
+	private String customerName;
+	@Column(unique = true)
+	private Long customerPhone;
+	private double totalBillAmount;
+	
+	@OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+	private List<Product> products;
 }
--- src/main/java/com/qsp/hotel_billing/dto/Item.java
+++ src/main/java/com/qsp/hotel_billing/dto/Item.java
@@ -1,5 +1,27 @@
 package com.qsp.hotel_billing.dto;
 
-public class Item {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Setter
+@Getter
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int itemId;
+	private String itemName;
+	private double itemPrice;
+	
+	@ManyToOne
+	@JoinColumn
+	private Menu menu;
+	
 }
